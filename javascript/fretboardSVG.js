49f638a97c2cf5cdce55ddbdfba6544953390ca8
David Potsiadlo <davidpots@gmail.com>
2014-03-12 11:40:11 -0400

    Adds intro text to index file

---
+++ javascript/fretboardSVG.js
@@ -0,0 +1,208 @@
+var dot = '<svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
+            <circle class="fretb_dot" fill="#27AAE1" cx="36" cy="44" r="30"/>\
+          </svg>';
+
+var dotWideLeft = '<svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
+                    	<path fill="#27AAE1" d="M36,16C20.537,16,8,28.537,8,44s12.537,28,28,28v0.125h36V16H36z"/>\
+                    </svg>';
+
+var dotWideRight = '<svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
+                    	<path fill="#27AAE1" d="M36,72.125c15.463,0,28-12.537,28-28s-12.537-28-28-28V16H0l0,56.125H36z"/>\
+                    </svg>';
+
+var dotWideMiddle = '<svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
+                 		    <rect y="16" fill="#27AAE1" width="72" height="56"/>\
+                      </svg>';
+
+var string_o = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                      width="100%" height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                  <g>\
+                     <path fill="#27AAE1" d="M26,24c5.514,0,10,4.486,10,10s-4.486,10-10,10s-10-4.486-10-10S20.486,24,26,24 M26,16\
+                     	c-9.94,0-18,8.06-18,18s8.06,18,18,18s18-8.06,18-18S35.94,16,26,16L26,16z"/>\
+                  </g>\
+                </svg>';
+
+var string_x = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                      width="100%" height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                  <path fill="#27AAE1" d="M41.067,43.831L31.236,34l9.831-9.831c-1.365-2.087-3.149-3.871-5.236-5.236L26,28.764l-9.831-9.831\
+                     c-2.087,1.365-3.871,3.149-5.236,5.236L20.764,34l-9.831,9.831c1.365,2.087,3.149,3.871,5.236,5.236L26,39.236l9.831,9.831\
+                     C37.918,47.702,39.702,45.918,41.067,43.831z"/>\
+                </svg>';
+
+var fretb_vert_4 = '<svg class="fretboard_svg" width="576" height="528" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                    	 viewBox="0 0 576 528" enable-background="new 0 0 576 528" xml:space="preserve">\
+                      <g class="fret_bg">\
+                    		<rect x="102" y="72" fill="#FFFFFF" width="372" height="384"/>\
+                      </g>\
+                      <g class="frets">\
+                    		<rect x="108" y="84" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="172" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="260" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="348" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="436" fill="#C7C8CA" width="360" height="8"/>\
+                      </g>\
+                      <g class="strings">\
+                    		<rect x="102" y="72" width="12" height="384"/>\
+                    		<rect x="174" y="72" width="12" height="384"/>\
+                    		<rect x="246" y="72" width="12" height="384"/>\
+                    		<rect x="318" y="72" width="12" height="384"/>\
+                    		<rect x="390" y="72" width="12" height="384"/>\
+                    		<rect x="462" y="72" width="12" height="384"/>\
+                      </g>\
+                      <g class="nut">\
+                      	<rect x="102" y="72" width="368" height="20"/>\
+                      </g>\
+                    </svg>';
+
+var fretb_vert_5 = '<svg class="fretboard_svg" width="576" height="616" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                    	 viewBox="0 0 576 616" enable-background="new 0 0 576 616" xml:space="preserve">\
+                      <g class="fret_bg">\
+                      	<rect x="102" y="72" fill="#FFFFFF" width="372" height="472"/>\
+                      </g>\
+                      <g class="frets">\
+                    		<rect x="108" y="84" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="172" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="260" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="348" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="436" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="524" fill="#C7C8CA" width="360" height="8"/>\
+                      </g>\
+                      <g class="strings">\
+                    		<rect x="102" y="72" width="12" height="472"/>\
+                    		<rect x="174" y="72" width="12" height="472"/>\
+                    		<rect x="246" y="72" width="12" height="472"/>\
+                    		<rect x="318" y="72" width="12" height="472"/>\
+                    		<rect x="390" y="72" width="12" height="472"/>\
+                    		<rect x="462" y="72" width="12" height="472"/>\
+                      </g>\
+                      <g class="nut">\
+                      	<rect x="102" y="72" width="368" height="20"/>\
+                      </g>\
+                    </svg>';
+
+var fretb_vert_7 = '<svg class="fretboard_svg" width="576" height="792" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                    	 viewBox="0 0 576 792" enable-background="new 0 0 576 792" xml:space="preserve">\
+                      <g class="fret_bg">\
+                    		<rect x="102" y="71" fill="#FFFFFF" width="372" height="649"/>\
+                      </g>\
+                      <g class="frets">\
+                    		<rect x="108" y="84" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="172" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="260" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="348" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="436" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="524" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="612" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="700" fill="#C7C8CA" width="360" height="8"/>\
+                      </g>\
+                      <g class="strings">\
+                    		<rect x="102" y="72" width="12" height="648"/>\
+                    		<rect x="174" y="72" width="12" height="648"/>\
+                    		<rect x="246" y="72" width="12" height="648"/>\
+                    		<rect x="318" y="72" width="12" height="648"/>\
+                    		<rect x="390" y="72" width="12" height="648"/>\
+                    		<rect x="462" y="72" width="12" height="648"/>\
+                      </g>\
+                      <g class="nut">\
+                      	<rect x="102" y="72" width="368" height="20"/>\
+                      </g>\
+                    </svg>';
+
+var fretb_vert_9 = '<svg class="fretboard_svg" width="576" height="968" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                    	 viewBox="0 0 576 968" enable-background="new 0 0 576 968" xml:space="preserve">\
+                      <g class="fret_bg">\
+                    		<rect x="102" y="72" fill="#FFFFFF" width="372" height="824"/>\
+                      </g>\
+                      <g class="frets">\
+                    		<rect x="108" y="84" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="172" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="260" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="348" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="436" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="524" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="612" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="700" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="788" fill="#C7C8CA" width="360" height="8"/>\
+                    		<rect x="108" y="876" fill="#C7C8CA" width="360" height="8"/>\
+                      </g>\
+                      <g class="strings">\
+                    		<rect x="102" y="72" width="12" height="824"/>\
+                    		<rect x="174" y="72" width="12" height="824"/>\
+                    		<rect x="246" y="72" width="12" height="824"/>\
+                    		<rect x="318" y="72" width="12" height="824"/>\
+                    		<rect x="390" y="72" width="12" height="824"/>\
+                    		<rect x="462" y="72" width="12" height="824"/>\
+                      </g>\
+                      <g class="nut">\
+                      	<rect x="102" y="72" width="368" height="20"/>\
+                      </g>\
+                    </svg>';
+
+var fretb_vert_12 = '<svg class="fretboard_svg" width="576" height="1232" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                      	 viewBox="0 0 576 1232" enable-background="new 0 0 576 1232" xml:space="preserve">\
+                        <g class="fret_bg">\
+                      		<rect x="102" y="72" fill="#FFFFFF" width="372" height="1088"/>\
+                        </g>\
+                        <g class="frets">\
+                      		<rect x="108" y="84" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="172" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="260" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="348" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="436" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="524" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="612" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="700" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="788" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="876" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="964" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="1052" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="1140" fill="#C7C8CA" width="360" height="8"/>\
+                        </g>\
+                        <g class="strings">\
+                      		<rect x="102" y="72" width="12" height="1088"/>\
+                      		<rect x="174" y="72" width="12" height="1088"/>\
+                      		<rect x="246" y="72" width="12" height="1088"/>\
+                      		<rect x="318" y="72" width="12" height="1088"/>\
+                      		<rect x="390" y="72" width="12" height="1088"/>\
+                      		<rect x="462" y="72" width="12" height="1088"/>\
+                        </g>\
+                        <g class="nut">\
+                        	<rect x="104" y="72" width="368" height="20"/>\
+                        </g>\
+                      </svg>';
+
+var fretb_vert_15 = '<svg class="fretboard_svg" width="576" height="1496" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
+                      	 viewBox="0 0 576 1496" enable-background="new 0 0 576 1496" xml:space="preserve">\
+                        <g class="fret_bg">\
+                      	  <rect x="102" y="72" fill="#FFFFFF" width="372" height="1352"/>\
+                        </g>\
+                        <g class="frets">\
+                      		<rect x="108" y="84" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="172" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="260" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="348" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="436" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="524" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="612" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="700" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="788" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="876" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="964" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="1052" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="1140" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="1228" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="1316" fill="#C7C8CA" width="360" height="8"/>\
+                      		<rect x="108" y="1404" fill="#C7C8CA" width="360" height="8"/>\
+                        </g>\
+                        <g class="strings">\
+                      		<rect x="102" y="72" width="12" height="1352"/>\
+                      		<rect x="174" y="72" width="12" height="1352"/>\
+                      		<rect x="246" y="72" width="12" height="1352"/>\
+                      		<rect x="318" y="72" width="12" height="1352"/>\
+                      		<rect x="390" y="72" width="12" height="1352"/>\
+                      		<rect x="462" y="72" width="12" height="1352"/>\
+                        </g>\
+                        <g class="nut">\
+                        	<rect x="104" y="72" width="368" height="20"/>\
+                        </g>\
+                      </svg>';
---
+++ javascript/main.js
@@ -0,0 +1,82 @@
+var fretboardHTML = '<h3 class="title"></h3><div class="fretboard_wrapper">\
+            <div class="fretboard_pillars"></div>\
+            <div class="fretboard_content"></div>\
+          </div>';
+
+var switchList = {  "o" : "<div class='cell dot'>"+dot+"</div>",
+                    "(" : "<div class='cell'>"+dotWideLeft+"</div>",
+                    ")" : "<div class='cell'>"+dotWideRight+"</div>",
+                    "=" : "<div class='cell'>"+dotWideMiddle+"</div>",
+                    "^" : "<div class='cell'>"+string_o+"</div>",
+                    "x" : "<div class='cell'>"+string_x+"</div>",
+                    "|" : "<div class='cell empty'></div>",
+                    " " : "<div class='cell empty'></div>" }
+
+var thisFretboard;
+
+$(document).ready(function(){
+
+  // For each Fretboard instance...
+  $.each( $('.fretboard_instance'), function(i,el) {
+
+    // Insert the HTML foundation
+
+          $(this).append(fretboardHTML);
+
+          // If the FB instance has a title, insert it above the FB
+          var fbTitle = $(this).find('.asciiFret').data('title');
+          if ( fbTitle != undefined ) {
+            $(this).find('.title').text(fbTitle);
+          }
+
+    // Read the ASCII fretboard that'll be replaced, then split it into an array of characters
+
+          var toReplace = $(el).find('.asciiFret').html();
+          toReplace = toReplace.split('');
+
+    // Go through each ASCII character...
+
+          var replacement = "";
+          $.each( toReplace, function(i,character) {
+            // Go through each item in SwitchList, if matches ASCII key then replace with SVG code
+            $.each( switchList, function( key, value ) {
+              if (character == key) {
+                replacement += value;
+              }
+            });
+          });
+
+    // Update the page with the new HTML/SVG markup
+
+          // Determine what size fretboard to use
+          var fretb_size,fretb_class;    
+          if ( $(this).find('.asciiFret').hasClass('v15') ) {
+            fretb_bg = fretb_vert_15;
+            fretb_class = "v15";
+          } else if ( $(this).find('.asciiFret').hasClass('v12')  ) {
+            fretb_bg = fretb_vert_12;
+            fretb_class = "v12";
+          } else if ( $(this).find('.asciiFret').hasClass('v9')  ) {
+            fretb_bg = fretb_vert_9;
+            fretb_class = "v9";
+          } else if ( $(this).find('.asciiFret').hasClass('v7')  ) {
+            fretb_bg = fretb_vert_7;
+            fretb_class = "v7";
+          } else if ( $(this).find('.asciiFret').hasClass('v5')  ) {
+            fretb_bg = fretb_vert_5;
+            fretb_class = "v5";
+          } else if ( $(this).find('.asciiFret').hasClass('v4')  ) {
+            fretb_bg = fretb_vert_4;
+            fretb_class = "v4";
+          }
+
+          // Fill the .fretboard with background, content, and a class
+          $(el).find('.fretboard_content')
+            .append(fretb_bg)
+            .append(replacement)
+            .parent().addClass(fretb_class);
+
+          // Remove the ASCII fretboard
+          $(this).find('.asciiFret').hide();
+  });
+});

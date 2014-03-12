a41f8beb4bec8f790e90edd447711fe1b559dff4
David Potsiadlo <davidpots@gmail.com>
2014-03-12 13:41:16 -0400

    removes width/height from all dots

--- javascript/fretboardSVG.js
+++ javascript/fretboardSVG.js
@@ -6,20 +6,20 @@ var dot = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="ht
             <circle class="fretb_dot" fill="#27AAE1" cx="36" cy="44" r="30"/>\
           </svg>';
 
-var dotWideLeft = '<svg width="72" height="88" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
+var dotWideLeft = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
                     	<path fill="#27AAE1" d="M36,16C20.537,16,8,28.537,8,44s12.537,28,28,28v0.125h36V16H36z"/>\
                     </svg>';
 
-var dotWideRight = '<svg width="72" height="88" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
+var dotWideRight = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
                     	<path fill="#27AAE1" d="M36,72.125c15.463,0,28-12.537,28-28s-12.537-28-28-28V16H0l0,56.125H36z"/>\
                     </svg>';
 
-var dotWideMiddle = '<svg width="72" height="88" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
+var dotWideMiddle = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 72 88" enable-background="new 0 0 72 88" xml:space="preserve">\
                  		    <rect y="16" fill="#27AAE1" width="72" height="56"/>\
                       </svg>';
 
 var string_o = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
-                      width="100%" height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                       viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
                   <g>\
                      <path fill="#27AAE1" d="M26,24c5.514,0,10,4.486,10,10s-4.486,10-10,10s-10-4.486-10-10S20.486,24,26,24 M26,16\
                      	c-9.94,0-18,8.06-18,18s8.06,18,18,18s18-8.06,18-18S35.94,16,26,16L26,16z"/>\
@@ -27,7 +27,7 @@ var string_o = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlin
                 </svg>';
 
 var string_x = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\
-                      width="100%" height="100%" viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
+                       viewBox="0 0 52 80" enable-background="new 0 0 52 80" xml:space="preserve">\
                   <path fill="#27AAE1" d="M41.067,43.831L31.236,34l9.831-9.831c-1.365-2.087-3.149-3.871-5.236-5.236L26,28.764l-9.831-9.831\
                      c-2.087,1.365-3.871,3.149-5.236,5.236L20.764,34l-9.831,9.831c1.365,2.087,3.149,3.871,5.236,5.236L26,39.236l9.831,9.831\
                      C37.918,47.702,39.702,45.918,41.067,43.831z"/>\
